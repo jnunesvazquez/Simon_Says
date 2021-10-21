73da0c7707b028b75921fa5e9523918d64bf07d7
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-10-21 14:30:30 +0200

    Adaptado el contador de rondas para aparecer cuando comience la ronda

--- app/src/main/java/com/joel/myapplication/MainActivity.kt
+++ app/src/main/java/com/joel/myapplication/MainActivity.kt
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var roundCounter : Int = 1
+    var secuence : Array<Int> = arrayOf(1, 2, 3, 4)
+    var comprobar : Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var roundCounter : Int = 0
-        var secuence = arrayOf(4)
+
         val startGame : Button = findViewById(R.id.play_button)
         startGame.setOnClickListener {
             Log.i("Estado", "Comenzando partida")
@@ -21,10 +27,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun showRound(){
         Log.i("Estado", "Mostrar numero de rondas")
+        val round : TextView = findViewById(R.id.round_number)
+        round.visibility = TextView.VISIBLE
+        round.text = roundCounter.toString()
     }
 
     private fun startSecuence(){
         Log.i("Estado", "Se ejecuta el juego")
+
+        while (comprobar == true){
+            roundCounter++
+        }
     }
 
     private fun userMessage(){
