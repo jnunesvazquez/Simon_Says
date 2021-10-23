74d9732bee15f2db85c60fedac9d3e0019ce0101
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-10-23 19:13:52 +0200

    Terminada la estructura de funciones principal del juego

--- app/src/main/java/com/joel/myapplication/MainActivity.kt
+++ app/src/main/java/com/joel/myapplication/MainActivity.kt
@@ -6,12 +6,19 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
     var roundCounter : Int = 1
-    var secuence : Array<Int> = arrayOf(1, 2, 3, 4)
+    var secuence = arrayOf<Int>()
     var comprobar : Boolean = true
+    val redButton : Button = findViewById(R.id.red_button)
+    val yellowButton : Button = findViewById(R.id.yellow_button)
+    val greenButton : Button = findViewById(R.id.green_button)
+    val blueButton : Button = findViewById(R.id.blue_button)
+    val round : TextView = findViewById(R.id.round_number)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,29 +29,37 @@ class MainActivity : AppCompatActivity() {
             Log.i("Estado", "Comenzando partida")
             showRound()
             startSecuence()
+            if (checkSecuence()){
+                roundCounter++
+                showRound()
+                startSecuence()
+            }else {
+                userMessage()
+                round.visibility = TextView.INVISIBLE
+                roundCounter = 1
+            }
         }
     }
 
     private fun showRound(){
         Log.i("Estado", "Mostrar numero de rondas")
-        val round : TextView = findViewById(R.id.round_number)
-        round.visibility = TextView.VISIBLE
+        if (round.visibility == TextView.INVISIBLE){
+            round.visibility = TextView.VISIBLE
+        }
         round.text = roundCounter.toString()
     }
 
     private fun startSecuence(){
         Log.i("Estado", "Se ejecuta el juego")
 
-        while (comprobar == true){
-            roundCounter++
-        }
     }
 
     private fun userMessage(){
         Log.i("Estado", "Mensaje por toast al usuario")
+        Toast.makeText(this, "Repetir secuencia", Toast.LENGTH_LONG).show()
     }
 
-    private fun checkSecuence(){
+    private fun checkSecuence() : Boolean{
         Log.i("Estado", "Comprobar que la secuencia del jugador coincide")
     }
 }
