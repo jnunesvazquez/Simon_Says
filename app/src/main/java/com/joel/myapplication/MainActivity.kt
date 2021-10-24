cc0b37e74e55a31337573cfca2e5922c43cf41a2
Joel Jorge Nunes Vázquez <jnunesvazquez@danielcastelao.org>
2021-10-24 12:27:40 +0200

    Secuencia inicializada y en funcionamiento con el cambio de colores de los botones (fallo en la visualizacion de la corutina)

--- app/src/main/java/com/joel/myapplication/MainActivity.kt
+++ app/src/main/java/com/joel/myapplication/MainActivity.kt
@@ -1,35 +1,50 @@
 package com.joel.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
-    var roundCounter : Int = 1
-    var secuence = arrayOf<Int>()
-    var comprobar : Boolean = true
-    val redButton : Button = findViewById(R.id.red_button)
-    val yellowButton : Button = findViewById(R.id.yellow_button)
-    val greenButton : Button = findViewById(R.id.green_button)
-    val blueButton : Button = findViewById(R.id.blue_button)
-    val round : TextView = findViewById(R.id.round_number)
+    var roundCounter : Int = 3
+    var selectColor : Array<Int> = arrayOf(1, 3, 3, 2)
+    lateinit var redButton : Button
+    lateinit var yellowButton : Button
+    lateinit var greenButton : Button
+    lateinit var blueButton : Button
+    lateinit var round : TextView
+
+    val redColor = Color.alpha(R.color.red)
+    val greenColor = Color.alpha(R.color.green)
+    val yellowColor = Color.alpha(R.color.yellow)
+    val blueColor = Color.alpha(R.color.blue)
+
+    val lightRedColor = Color.parseColor("#FF4B4B")
+    val lightGreenColor = Color.parseColor("#93FF4B")
+    val lightYellowColor = Color.parseColor("#FFFE00")
+    val lightBlueColor = Color.parseColor("#00FFF9")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        redButton = findViewById(R.id.red_button)
+        yellowButton = findViewById(R.id.yellow_button)
+        greenButton = findViewById(R.id.green_button)
+        blueButton = findViewById(R.id.blue_button)
+        round = findViewById(R.id.round_number)
         val startGame : Button = findViewById(R.id.play_button)
         startGame.setOnClickListener {
             Log.i("Estado", "Comenzando partida")
             showRound()
             startSecuence()
-            if (checkSecuence()){
+            /*if (checkSecuence()){
                 roundCounter++
                 showRound()
                 startSecuence()
@@ -37,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 userMessage()
                 round.visibility = TextView.INVISIBLE
                 roundCounter = 1
-            }
+            }*/
         }
     }
 
@@ -51,15 +66,50 @@ class MainActivity : AppCompatActivity() {
 
     private fun startSecuence(){
         Log.i("Estado", "Se ejecuta el juego")
-
+        runBlocking {
+            launch {
+                //selectColor += (1..5).random()
+                for (i : Int in 1..roundCounter){
+                    when (selectColor[i]) {
+                        1 -> {
+                            greenButton.setBackgroundColor(lightGreenColor)
+                            delay(2000)
+                            greenButton.setBackgroundColor(lightRedColor)
+                            delay(1000)
+                        }
+                        2 -> {
+                            redButton.setBackgroundColor(lightRedColor)
+                            delay(2000)
+                            redButton.setBackgroundColor(lightGreenColor)
+                            delay(1000)
+                        }
+                        3 -> {
+                            blueButton.setBackgroundColor(lightBlueColor)
+                            delay(2000)
+                            blueButton.setBackgroundColor(lightYellowColor)
+                            delay(1000)
+                        }
+                        4 -> {
+                            yellowButton.setBackgroundColor(lightYellowColor)
+                            delay(2000)
+                            yellowButton.setBackgroundColor(lightBlueColor)
+                            delay(1000)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private fun userMessage(){
         Log.i("Estado", "Mensaje por toast al usuario")
-        Toast.makeText(this, "Repetir secuencia", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Has cometido un error", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Empieza de nuevo", Toast.LENGTH_LONG).show()
     }
 
     private fun checkSecuence() : Boolean{
         Log.i("Estado", "Comprobar que la secuencia del jugador coincide")
+        Toast.makeText(this, "Repetir secuencia", Toast.LENGTH_LONG).show()
+        return true
     }
 }
